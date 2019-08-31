ab768fc16d1a0d461a35c69630c80c126d6c2754
Eric Dang <edang830@gmail.com>
2019-08-30 19:26:46 -0700

    Initial Commit

---
+++ brew.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# install languages
+brew install python3
+brew install python2
+
+# install tools
+brew install tree
+brew install git
+brew install parallel
+brew install rename
+# brew install open-babel
+
+# install programs
+brew cask install google-chrome
+brew cask install visual-studio-code
+brew cask install miniconda
+brew cask install google-drive-file-stream
