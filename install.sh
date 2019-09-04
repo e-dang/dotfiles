05435a89a48ef767073942300dd4f921695e30dd
Eric Dang <edang830@gmail.com>
2019-09-03 21:12:59 -0700

    add install script

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+############################
+# This script creates symlinks from the home directory to any desired dotfiles in ${homedir}/.dotfiles
+# And also installs Homebrew Packages
+# And sets vscode preferences
+# And sets MacOS preferences
+###########################
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: install.sh <home_directory>"
+    exit 1
+fi
+
+homedir=$1
+
+# dotfiles directory
+dotfiledir=${homedir}/.dotfiles
+
+# list of files/folders to symlink in ${homedir}
+files="bash_profile bashrc bash_prompt aliases env functions hushlogin pylintrc inputrc"
+
+# change to the dotfiles directory
+echo "Changing to the ${dotfiledir} directory"
+cd ${dotfiledir}
+echo "...done"
+
+# create symlinks (will overwrite old dotfiles)
+for file in ${files}; do
+    echo "Creating symlink to $file in home directory."
+    ln -sf ${dotfiledir}/.${file} ${homedir}/.${file}
+done
+
+# Run the Homebrew Script
+./brew.sh
+
+# Run the MacOS Script
+./macos.sh
+
+# Run the VSCode Script
+./vscode.sh
