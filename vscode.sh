a2e8b3466d281af55bdf65daff93e77ba71956e7
Eric Dang <edang830@gmail.com>
2019-09-03 21:12:37 -0700

    add vscode settings install script

---
+++ vscode.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# copy user settings
+cp ~/.dotfiles/settings/vscode/keybindings.json /Users/ericdang/Library/Application\ Support/Code/User/
+cp ~/.dotfiles/settings/vscode/settings.json /Users/ericdang/Library/Application\ Support/Code/User/
