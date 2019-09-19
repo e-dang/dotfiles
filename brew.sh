772d3ff67f2d550e56607f5e500e6b3b50863916
Eric Dang <edang830@gmail.com>
2019-09-19 14:09:28 -0700

    change divvy to shiftit

--- brew.sh
+++ brew.sh
@@ -19,7 +19,7 @@ brew cask install google-chrome
 brew cask install visual-studio-code
 brew cask install miniconda
 brew cask install google-backup-and-sync
-brew cask install divvy
+brew cask install shiftit
 brew cask install iterm2
 brew cask install mongodb
 brew cask install mongodb-compass
