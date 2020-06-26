25b10853f719b3134353d54fdb86b2e5c02f37de
Eric Dang <edang830@gmail.com>
2020-06-26 13:33:03 -0700

    Add env variables for zsh completions

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -1,8 +1,7 @@
-
-
 # Better history
 HISTFILE="$HOME/.zsh_history"
 SAVEHIST=1000000
+
 bindkey "^R" history-incremental-search-backward
 bindkey '\e[A' history-search-backward
 bindkey '\e[B' history-search-forward
@@ -13,6 +12,10 @@ setopt histfindnodups
 setopt histignoredups
 
 # Tab to cycle through auto-completions like in oh-my-zsh
+CASE_SENSITIVE=false
+HYPHEN_INSENSITIVE=true
+ZSH_CACHE_DIR="$HOME/.zsh/cache"
+
 bindkey "\e[Z" menu-complete
 autoload -Uz compinit && compinit -i
 source ~/.zsh/completion.zsh
