d63b9d7909594d820f212e855c5a283a7bc8318d
Eric Dang <edang830@gmail.com>
2020-06-27 16:26:01 -0700

    Update path conditionally

--- shell/path.sh
+++ shell/path.sh
@@ -1,4 +1,10 @@
-# update PATH to point to homebrew python3
-PATH=/usr/local/opt/python/libexec/bin:$PATH
+# If homebrew installs python3, update PATH so it is the default python
+if [ -d "/usr/local/opt/python/libexec/bin" ]; then
+    path_prepend /usr/local/opt/python/libexec/bin
+fi
 
-export PATH
+
+# If homebrew installs llvm, update PATH so it is the default clang
+if [ -d "/usr/local/opt/llvm/bin" ]; then
+    path_prepend /usr/local/opt/llvm/bin
+fi
