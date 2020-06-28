233d2f6f39e09b779ff12038fb4ee35c5a1738e8
Eric Dang <edang830@gmail.com>
2020-06-27 18:38:29 -0700

    Fix bug with env variable not being evaluated

--- shell/env.sh
+++ shell/env.sh
@@ -1,7 +1,8 @@
 export SAVEHIST=1000000
 
 # Color ls output better than default but worse than if using dircolors
-if [ ${USING_DIR_COLORS} = false ]; then
+if [ "${USING_DIR_COLORS}" = false ]; then
     export CLICOLOR=1
     export LSCOLORS='exbxgxdxcxGxDxcxcxexex'
 fi
+unset USING_DIR_COLORS
--- shell/plugins.sh
+++ shell/plugins.sh
@@ -1,12 +1,12 @@
 # dircolors
+export USING_DIR_COLORS=false
 if [[ "$(tput colors)" == "256" ]]; then
     COLOR_FILE="~/.shell/plugins/dircolors-solarized/dircolors.256dark"
-    export USING_DIR_COLORS=true
     if [ -x "$(command -v dircolors)" ]; then # linux command
         eval $(dircolors "${COLOR_FILE}")
+        USING_DIR_COLORS=true
     elif [ -x "$(command -v gdircolors)" ]; then # macos command if coreutils is installed and in path
         eval $(gdircolors "${COLOR_FILE}")
-    else
-        USING_DIR_COLORS=false
+        USING_DIR_COLORS=true
     fi
 fi
