226fc30c8d92a96411143fe76c819f883bc06b55
Eric Dang <edang830@gmail.com>
2020-06-27 19:59:50 -0700

    Add aliases for GNU commands on macos

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -4,11 +4,19 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
-# always enable colored `ls` output with human readable sizes
-alias ls='ls -Gh'
+if [[ $(brew list | grep "coreutils") ]] 2> /dev/null; then
+    # always enable colored `ls` output with human readable sizes
+    alias ls='gls -Gh'
 
-# ls command shortcut
-alias l='ls -lah'
+    # ls command shortcut
+    alias l='gls -lah'
+else
+    # always enable colored `ls` output with human readable sizes
+    alias ls='ls -Gh'
+
+    # ls command shortcut
+    alias l='ls -lah'
+fi
 
 # Always enable colored `grep` output
 alias grep='grep --color=auto'
