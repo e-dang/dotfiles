dbb3886accf1005ecb96c0e48be0c93f682175a3
Eric Dang <edang830@gmail.com>
2020-06-28 18:05:54 -0700

    Switch to directory checking because its faster

--- shell/env.sh
+++ shell/env.sh
@@ -8,17 +8,17 @@ fi
 unset USING_DIR_COLORS
 
 # Add compiler flags for using brew llvm as default compiler. TAG:CLANG_COMPILER
-if [[ $(brew list | grep "llvm")  ]] 2> /dev/null; then
-    LDFLAGS += -L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib
-    CPPFLAGS += -I/usr/local/opt/llvm/include -I/usr/local/opt/llvm/include/c++/v1/
+if [[ -d /usr/local/opt/llvm/bin  ]]; then
+    export LDFLAGS="-L/usr/local/opt/llvm/lib -Wl,-rpath,/usr/local/opt/llvm/lib"
+    export CPPFLAGS="-I/usr/local/opt/llvm/include -I/usr/local/opt/llvm/include/c++/v1/"
     export CC=$(which clang)
     export CXX=$(CC)++
 fi
 
 # If homebrew installs gcc, update env vars so it is the default gcc. Note - may need to update paths depending on version. TAG:GCC_COMPILER
-if [[ $(brew list | grep "gcc")  ]] 2> /dev/null; then
-    LDFLAGS += -L/usr/local/opt/gcc/lib/gcc/9 -Wl,-rpath,/usr/local/opt/gcc/lib/gcc/9
-    CPPFLAGS += -I/usr/local/opt/gcc/include -I/usr/local/opt/gcc/include/c++/9.2.0/
-    export CC=$(which gcc)
-    export CXX=$(CC)++
-fi
+# if [[ -d /usr/local/opt/gcc/bin  ]]; then
+#     export LDFLAGS="-L/usr/local/opt/gcc/lib/gcc/9 -Wl,-rpath,/usr/local/opt/gcc/lib/gcc/9"
+#     export CPPFLAGS="-I/usr/local/opt/gcc/include -I/usr/local/opt/gcc/include/c++/9.2.0/"
+#     export CC=$(which gcc)
+#     export CXX=$(CC)++
+# fi
