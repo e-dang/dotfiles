44670f62ab379e2b543fe98fe06c29f18c1ee799
Eric Dang <edang830@gmail.com>
2020-06-29 19:44:19 -0700

    Fix change zsh history options to match omz

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -11,11 +11,13 @@ setopt prompt_subst
 bindkey "^R" history-incremental-search-backward
 bindkey '\e[A' history-search-backward
 bindkey '\e[B' history-search-forward
-setopt appendhistory
-setopt incappendhistory
-setopt extendedhistory
-setopt histfindnodups
-setopt histignoredups
+setopt extended_history       # record timestamp of command in HISTFILE
+setopt hist_find_no_dups      # dont report back duplicate commands
+setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_verify            # show command with history expansion to user before running it
+setopt share_history          # share command history data
 
 # Tab to cycle through auto-completions like in oh-my-zsh
 bindkey "\e[Z" menu-complete
