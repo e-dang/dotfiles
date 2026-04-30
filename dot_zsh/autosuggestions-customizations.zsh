da4b47286d0e883401fddd96d8e38397b3d640e3
Eric Dang <edang830@gmail.com>
2026-04-29 23:50:07 -0700

    Fix autocomplete bug

--- dot_zsh/autosuggestions-customizations.zsh
+++ dot_zsh/autosuggestions-customizations.zsh
@@ -1,5 +1,19 @@
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#4c4c4c'
-bindkey '^[OC' forward-char
+
+# Right arrow: if an autosuggestion is showing, accept one word; otherwise
+# move a single character. POSTDISPLAY holds the suggestion text when active.
+_smart_forward() {
+  if [[ -n $POSTDISPLAY ]]; then
+    zle forward-word
+  else
+    zle forward-char
+  fi
+}
+zle -N _smart_forward
+ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS+=(_smart_forward)
+bindkey '^[OC' _smart_forward
+bindkey '^[[C' _smart_forward
+
 bindkey '\e[1;2C' forward-char
 bindkey '\e[1;2D' backward-kill-word
 
